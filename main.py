a7c107332253b7b298fa9558bef5810100e1dc89
iyurcl <susiiexilli@gmail.com>
2025-05-25 14:10:10 +0300

    الملفات الأولية لمشروع MakeCode

---
+++ main.py
@@ -0,0 +1,7 @@
+basic.show_string("Hello!")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
+basic.show_string("SADEEM!")
+basic.show_icon(IconNames.HEART)
---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.showString("Hello!")
+basic.forever(function on_forever() {
+    
+})
+basic.showString("SADEEM!")
+basic.showIcon(IconNames.Heart)
